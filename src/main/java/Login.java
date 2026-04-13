818b3cbc8cd990259cd2dbed7f2cd725348805e4
PNettra <120159010+PNettra@users.noreply.github.com>
2026-04-12 22:29:33 -0700

    Create initial Login interface

---
+++ src/main/java/Login.java
@@ -0,0 +1,20 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+/**
+ * Interface for Login scene
+ *
+ * @author Nettra Leandre
+ * @version 0.1.0
+ * @since 4/12/2026
+ */
+
+public interface Login {
+
+}
