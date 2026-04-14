c5e214ee283e37b0cbfda35db2f44729940f7358
smc-cmyk <smcferran@csumb.edu>
2026-04-14 12:49:49 -0700

    Initial creation of HistoricTableScene and HistoricTableSceneTest.

---
+++ src/main/java/HistoricTableScene.java
@@ -0,0 +1,15 @@
+package main.java;
+import javafx.application.Application;
+
+ /**
+ * Takes currency conversion historic data and creates a
+ * scene that displays the data as a table.
+ *
+ * @author Sean McFerran
+ * @version 0.1.0
+ * @since 04/14/2026
+ */
+
+public class HistoricTableScene extends Application {
+
+}
---
+++ src/test/java/HistoricTableSceneTest.java
@@ -0,0 +1,15 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for historic table scene.
+ *
+ * @author Sean McFerran
+ * @version 0.1.0
+ * @since 04/14/2026
+ */
+
+class HistoricTableSceneTest {
+
+}
