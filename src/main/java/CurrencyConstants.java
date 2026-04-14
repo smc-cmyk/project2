b4b96b059b7cdd6400eebc7205e07e0d7a5ab2ba
smc-cmyk <smcferran@csumb.edu>
2026-04-14 10:44:44 -0700

    Created initial CurrencyConstants file and populated it. CurrencyConverters created but empty.

---
+++ src/main/java/CurrencyConstants.java
@@ -0,0 +1,13 @@
+public class CurrencyConstants {
+
+    //TIMESTAMP FOR DATA RETRIEVAL
+    public static final String dateOfRetrieval = "April 14, 2026, 10:00AM PST";
+
+    //ONE US DOLLAR IS EQUAL TO...
+    public static final double usdToEuro = 0.85;
+    public static final double usdToYen = 158.72;
+    public static final double usdToPound = 0.74;
+    public static final double usdToYuan = 6.82;
+    public static final double usedToCad = 1.38;
+
+}
