f4a2c005a47e6fc133e069346a1725a8b9c34174
penakevin235 <penakevin235.com>
2026-04-15 18:24:39 -0700

    changed the layout for SceneFactory and made SceneTypes enum

--- src/main/java/SceneFactory.java
+++ src/main/java/SceneFactory.java
@@ -2,43 +2,39 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * [Description]
+ * [each scene should be made in their respective class.]
  *
  * @author Kevin Pena Alvarez
  * @version 0.1.0
  * @since 4/14/26
  **/
 public class SceneFactory {
-    public static final int LOGIN = 1;
-    public static final int MAIN_MENU = 2;
-    public static final int HISTORIC_TABLE = 3;
-    public static final int HISTORIC_GRAPH = 4;
-    public static final int REAL_TIME = 5;
-    public static final int CONVERTER_MENU = 6;
-    public static final int CONVERTER_RESULTS = 7;
-    public static final int INSTRUCTIONS = 8;
-    public static final int LOGOUT = 9;
 
-    //this is the method that switches the scene
-    public static void switchScene(Stage stage, int sceneId) {
-        Scene scene = getScene(sceneId, stage); //retrieves scenes
-        stage.setScene(scene);
-    }
+    public Scene create (SceneType sceneType, Stage stage) {
+        switch(sceneType) {
+           /** case LOGIN:
+                return LoginScene.create(stage);
+            case MAIN_MENU:
+                return MainMenuScene.create(stage);
+            case EXCHANGE_RATE_HISTORY:
+                return ExchangeRateHistoryScene.create(stage);
+            case TABLE_OF_EXCHANGE_RATE_HISTORY:
+                return TableOfExchangeHistoryScene.create(stage);
+            case GRAPH_OF_EXCHANGE_RATE_HISTORY:
+                return GraphOfExchangeHistoryScene.create(stage);
+            case TABLE_OF_EXCHANGE_RATE_HISTORY_WITH_TIMESTAMPS:
+                return TableOfExchangeHistoryWithTimestampsScene.create(stage);
+            case US_TO_:
+                return UStoScene.create(stage);
+            case INSTRUCTIONS:
+                return InstructionScene.create(stage);
+            case LOGOUT:
+                return LogoutScene.create(stage);
+            */
+            default:
+                throw new IllegalArgumentException("Unknown scene type: " + sceneType );
+        }
 
-    //Scenes still need to be made
-    private static Scene getScene(int sceneId, Stage stage) {
-        return switch (sceneId) {
-            case LOGIN -> LoginScene.create(stage);
-            case MAIN_MENU -> MainMenuScene.create(stage);
-            case HISTORIC_TABLE -> HistoricTableScene.create(stage);
-            case HISTORIC_GRAPH -> GraphScene.create(stage);
-            case REAL_TIME -> RealtimeScene.create(stage);
-            case CONVERTER_MENU -> ConvertScene.create(stage);
-            case CONVERTER_RESULTS -> ResultScene.create(stage);
-            case INSTRUCTIONS -> InstructionsScene.create(stage);
-            case LOGOUT -> LogoutScene.create(stage);
-            default -> throw new IllegalArgumentException("Invalid scene");
-        };
     }
 }
 
