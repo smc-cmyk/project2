8b0c727f74b8448803622b06cac8baafe0e351fa
Linda <linnguyen@csumb.edu>
2026-04-18 11:30:43 -0700

    Fixed code to run tests and rename the file name to match what is in SceneFactory

---
+++ src/main/java/InstructionsScene.java
@@ -0,0 +1,58 @@
+/*
+* Interface for Instructions Scene
+*
+* @author: Linda Nguyen
+* @since: 4/18/2026
+*
+ */
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class InstructionsScene {
+    // UI Strings
+    private static final String title = "Instructions";
+    private static final String instructions =
+            "Click on each button to navigate to desired scene \n\n" +
+            "Enter desired USD amount to convert, then click desired currency to convert to \n\n" +
+            "Click 'Return' to go to the previous scene";
+
+    // Back button String
+    private static final String back = "Return";
+
+    private static Label titleLabel;
+    private static Label instructionsLabel;
+    private static Button backButton;
+
+    public static Scene create(Stage stage) {
+
+        titleLabel = new Label(title);
+        instructionsLabel = new Label(instructions);
+        backButton = new Button(back);
+
+        // Back button to go back to the Main Menu
+        backButton.setOnAction(e -> {
+            SceneFactory.switchScene(stage, SceneFactory.MAIN_MENU);
+        });
+
+        // Created the VBox
+        VBox vBox = new VBox(10, titleLabel, instructionsLabel, backButton);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setSpacing(10);
+
+        // Display the scene with dimensions
+        Scene scene = new Scene(vBox, 500, 500);
+        return scene;
+    }
+
+    public static Label getInstructionsLabel() {
+        return instructionsLabel;
+    }
+    public static Button getBackButton() {
+        return backButton;
+    }
+}
