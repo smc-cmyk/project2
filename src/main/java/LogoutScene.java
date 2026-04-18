c95b37cdf0e47a9d9958ca5b0ff5aa646cdd31ca
Linda <linnguyen@csumb.edu>
2026-04-18 12:42:07 -0700

    Create UI Strings

--- src/main/java/LogoutScene.java
+++ src/main/java/LogoutScene.java
@@ -1,3 +1,5 @@
+import javafx.fxml.FXML;
+
 /*
  * Class for Logout Scene
  *
@@ -5,5 +7,28 @@
  * @since: 4/18/2026
  *
  */
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 public class LogoutScene {
+    private static final String logout = "Logout";
+    private static final String check = "Are you sure you want to logout?";
+    private static final String yes = "Yes";
+    private static final String no = "No";
+
+    private static Label logoutLabel;
+    private static Label checkLabel;
+    private static Label yesButton;
+    private static Label noButton;
+
+    public static Scene create(Stage stage) {
+
+
+    }
+
 }
