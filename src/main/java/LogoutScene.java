f6c2420959b1ddcd1781fc00c090b6d92e49d8f2
Linda <linnguyen@csumb.edu>
2026-04-18 22:34:27 -0700

    Updated the login screen to include a dialog box / alert box showing user logged out.

--- src/main/java/LogoutScene.java
+++ src/main/java/LogoutScene.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXML;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -40,6 +41,13 @@ public class LogoutScene {
 
         // Yes button takes you to the login scene
         yesButton.setOnAction(e -> {
+            // Custom Dialog Box showing user logged out before going to the login screen
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Logged Out");
+            alert.setHeaderText(null);
+            alert.setContentText("You're logged out.");
+            alert.showAndWait();
+
             stage.setScene(factory.create(SceneType.LOGIN, stage));
         });
 
