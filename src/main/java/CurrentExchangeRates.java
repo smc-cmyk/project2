8ea97da48485c748db38e57883db90ffe985c266
smc-cmyk <smcferran@csumb.edu>
2026-04-20 13:29:40 -0700

    Initial setup for CurrentExchangeRates completed, now to add the data.

--- src/main/java/CurrentExchangeRates.java
+++ src/main/java/CurrentExchangeRates.java
@@ -1,6 +1,7 @@
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -21,9 +22,25 @@ public class CurrentExchangeRates {
 
     public static Scene create (Stage stage) {
         SceneFactory sceneFactory = new SceneFactory();
+        stage.setTitle(title + " - " + timestamp);
 
         headerLabel = new Label(title);
         dateLabel = new Label(timestamp);
+
         returnButton = new Button("Return to Main Menu");
+
+        VBox root = new VBox(headerLabel, dateLabel, returnButton);
+
+        Scene scene = new Scene(root);
+
+        //Return to main menu
+        returnButton.setOnAction(e -> {
+            stage.setScene(sceneFactory.create(SceneType.MAIN_MENU,stage));
+        });
+
+        stage.setScene(scene);
+        stage.show();
+
+        return scene;
     }
 }
