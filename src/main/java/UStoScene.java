20be6ce04ab61c97c52536cd3bc4e8381e678d51
PNettra <120159010+PNettra@users.noreply.github.com>
2026-04-21 23:12:24 -0700

    Add UStoScene to avoid conflicts

---
+++ src/main/java/UStoScene.java
@@ -0,0 +1,30 @@
+import java.util.Objects;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+/**
+ * [This is the Scene for conversion from USD to supported currencies. ]
+ *
+ * @author Kevin Pena Alvarez
+ * @version 0.1.0
+ * @since 4/15/26
+ **/
+public class UStoScene {
+  public static Scene create(Stage stage) {
+    try {
+      Parent root = FXMLLoader.load(
+          Objects.requireNonNull(
+              UStoScene.class.getResource("/UStoScene.fxml")
+          )
+      );
+
+      return new Scene(root, 400, 350);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      return new Scene(new Label("Error loading scene"), 400, 350);
+    }
+  }}
