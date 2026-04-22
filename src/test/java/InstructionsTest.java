9bf0f6bebaa62b6ba60d2345ecf60f40a82af213
PNettra <120159010+PNettra@users.noreply.github.com>
2026-04-21 22:59:14 -0700

    Modify InstructionsTest to include InstructionsScene

--- src/test/java/InstructionsTest.java
+++ src/test/java/InstructionsTest.java
@@ -2,14 +2,17 @@
  * Instructions Unit test for Instructions Scene
  *
  * @author: Linda Nguyen
- * @since: 4/16/2026
+ * @since: 4/18/2026
  *
  */
 
 import javafx.application.Platform;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,21 +24,27 @@ class InstructionsTest {
         Platform.startup(() -> {});
     }
 
-    //Check for title to ensure information is flowing through
+    // Check for first word of instructions
     @Test
     void testInstructions() {
-        Instructions.create(new Stage());
+        InstructionsScene.create(new Stage());
 
-        assertNotNull(Instructions.instructions);
-        assertTrue(Instructions.instructions.getText().contains("Click"));
+        Label instructions = InstructionsScene.getInstructionsLabel();
+
+        // Test to ensure instructions not null and has the text we are looking for
+        assertNotNull(instructions);
+        assertTrue(instructions.getText().contains("Click"));
     }
 
-    //Verify the back button is available
+    // Verify the back button is available
     @Test
     void testBackButton() {
-        Instructions.create(new Stage());
-        assertNotNull(Instructions.back);
-        assertEquals("Back", Instructions.back.getText());
+        InstructionsScene.create(new Stage());
+
+        Button back = InstructionsScene.getBackButton();
+
+        assertNotNull(back);
+        assertEquals("Return", back.getText());
     }
 
 }
